f74cb2b7e5527fd578e2cbb8ab459d758b295f64
Rikuto <orikutobasuke@gmail.com>
2023-01-31 15:36:55 +0900

    gurappu add

---
+++ public/js/teacher_01.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById("btn");
+const text = document.getElementById("text");
+const pas = document.getElementById("pas");
+const file = "acount.json";
+fetch(file)
+    .then(response => response.json())
+    .then(json=>{
+        btn.addEventListener("click",()=>{
+            json.forEach((e) => {
+                if(e.name === text.value && e.pas === pas.value){
+                    location.href = "teacher_02.html"
+                }
+                else{
+                    alert("内容が正しくありません。")
+                }
+            });
+        })
+    });
